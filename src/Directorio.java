4f406d27c3327dbdf3a2eefc1d0c37e0051f1ba5
JoaquinOrtega505 <72822153+JoaquinOrtega505@users.noreply.github.com>
2023-05-13 16:44:47 -0300

    claseDirectorio

--- src/Directorio.java
+++ src/Directorio.java
@@ -1,13 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Mateo
- */
-class Directorio {
+package tp6.negocio;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import javax.swing.JOptionPane;
+
+// buscarTeléfono() que en base a un apellido nos devuelve una lista con los nros de 
+//teléfono asociados a dicho apellido.
+public class Directorio {
+    private HashMap<String,Cliente> registro=new HashMap<String,Cliente>();
     
+    public Cliente buscarCliente(String numTelefono){
+             
+        return registro.get(numTelefono);
+    }
+public ArrayList<Cliente> buscarClientes(String ciudad){
+        
+        ArrayList<Cliente>buscar= new ArrayList();
+        Set<String> claves=registro.keySet();
+       Iterator<String> it = claves.iterator();
+       while(it.hasNext()){
+           String claveActual=it.next();
+           Cliente clienteActual=registro.get(claveActual);
+           
+           if(clienteActual.getCiudad().equals(ciudad)){
+               buscar.add(clienteActual);
+           }         
+       }
+        return buscar;
+    }
+
+
+
+//AgregarCliente() que permite registrar un nuevo cliente con su respectivo nro de 
+//teléfono. Siendo el nro del teléfono la clave del mismo.
+    public boolean agregarCliente(String telefono, Cliente c) {
+        boolean agregado = false;
+        if (!registro.containsKey(telefono)) {
+            registro.put(telefono, c);
+            agregado = true;
+        }
+        return agregado;
+    }   
+    
+    public ArrayList<String> buscarTelefono(String apellido){
+        ArrayList <String> num= new ArrayList<String>();
+        for (Map.Entry<String, Cliente> entry : registro.entrySet()) {
+            if(registro.containsValue(apellido)){
+                num.add(entry.getKey());
+            }
+        }
+        //hola
+//        Iterator <String> i= num.iterator();
+//        while(i.hasNext()){
+//            System.out.println(i.next());
+//        }
+        return num;
+    }
+    
+    public void borrarCliente(String telefono){
+        Cliente result = registro.remove(telefono);
+        if (result != null) {
+            JOptionPane.showMessageDialog(null,"Cliente eliminado");
+        } else {
+            JOptionPane.showMessageDialog(null,"No se pudo borrar el cliente");
+        }
+    }
 }
